c01b471b243d3a8fd6a7a056175aea9485c5588a
RokasV <vezikauskas.r@gmail.com>
2023-11-29 08:59:05 +0200

    init

---
+++ js/callback.js
@@ -0,0 +1,39 @@
+"use strict";
+console.log("callback.js file was loaded");
+
+// setTimeout(() => {
+//   // confirm("ar tirkai?");
+// }, 5000);
+
+// function sum(n1, n2, callBack) {
+//   let suma = n1 + n2;
+//   callBack(suma);
+// }
+// function makeCurrency(val) {
+//   console.log(`$${val.toFixed(2)}`);
+// }
+// sum(15, 20, makeCurrency);
+
+function getHeader(callBack) {
+  setTimeout(() => {
+    console.log("<Header/>");
+    callBack();
+  }, 3000);
+}
+function getContent(callBack) {
+  setTimeout(() => {
+    console.log("<Main Content/>");
+    callBack();
+  }, 1000);
+}
+function getFooter() {
+  setTimeout(() => {
+    console.log("<Footer/>");
+  }, 1000);
+}
+
+getHeader(() => {
+  getContent(() => {
+    getFooter();
+  });
+});
---
+++ js/promise.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("promise.js file was loaded");
+
+// Pirksim lektuvo bilietus
+
+// galimi 3 variantai
+// pirkimo rezultatas
+// vykdomas - pending
+// pavyko - Resolve
+// neapvyko - Reject
+
+let pirkimoPazadas = new Promise((resolve, reject) => {});
